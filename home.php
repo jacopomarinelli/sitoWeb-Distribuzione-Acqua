834a3c655bca1a9d8db61536cf6b2957d870751c
jacopomarinelli <j.marinelli1@studenti.unibg.it>
2026-05-15 22:46:01 +0200

    Aggiornamento pagina.css e home.php

--- home.php
+++ home.php
@@ -1,9 +1,18 @@
 <?php
 include 'header.php';
 ?>
+<!-- Qui va inserito codice pagina -->
+        
+        <div class="main">
+            <div class="aside">
+                <p>Cerca</p>
+            </div>
+            <div class="section">
+                <p>Mostra</p>
+            </div>
+        </div>
 
 <!-- Qui va inserito codice pagina -->
-
 <?php
 include 'footer.php';
 ?>
