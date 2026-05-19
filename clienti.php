d59e3d4615d2050c572889f51e19e6bf6363c9a5
DanieleSalogni <d.salogni@studenti.unibg.it>
2026-05-19 17:22:36 +0200

    Upload cliente
    - prima formattazione della pagina cliente

---
+++ clienti.php
@@ -0,0 +1,18 @@
+<?php
+include 'header.php';
+?>
+<!-- Qui va inserito codice pagina -->
+        
+        <div class="cliente">
+            <form action="" method="GET" class="ricerca">
+                <label>Parola chiave</label>
+                <input type="text" name="keyword">
+
+                <button type="submit">cerca</button>
+            </form>
+        </div>
+
+<!-- Qui va inserito codice pagina -->
+<?php
+include 'footer.php';
+?>
