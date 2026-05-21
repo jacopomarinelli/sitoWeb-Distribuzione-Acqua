c7b03b5bd18ef2ee7f3ca9719623063f14854a92
jacopomarinelli <j.marinelli1@studenti.unibg.it>
2026-05-21 18:25:07 +0200

    Aggiornamento pagina clienti

--- clienti.php
+++ clienti.php
@@ -6,35 +6,34 @@ include 'header.php';
         <div class="cliente">
 
             <form action="" method="GET" class="ricerca">
+                <p class="campo">Inserisci i dati per la ricerca</p>
 
-            <div class="campo">
-                <label><h3>Codice: </h3></label>
-                <input type="text" name="codice" class="textarea">
-            </div>
-            <div class="campo">
-                <label><h3>Codice fiscale: </h3></label>
-                <input type="text" name="cod_fis" class="textarea">
-            </div>
-            <div class="campo">
-                <label><h3>Ragione sociale: </h3></label>
-                <input type="text" name="rag_soc" class="textarea">
-            </div>
-            <div class="campo">
-                <label><h3>Indirizzo: </h3></label>
-                <input type="text" name="indirizzo" class="textarea">
-            </div>
-            <div class="campo">
-                <label><h3>Città: </h3></label>
-                <input type="text" name="città" class="textarea">
-            </div>
-            <div class="avvio-ricerca">
-                <button type="submit">CERCA</button>
-            </div>
+                <label for="cod" class="campo">Codice: </label>
+                <input type="text" id="cod" name="codice" class="campo">
+                
+                <label for="cod_fis" class="campo">Codice fiscale: </label>
+                <input type="text" id="cod_fis" name="cod_fis" class="campo">
+                
+                <label for="rag_soc" class="campo">Ragione sociale: </label>
+                <input type="text" id="rag_soc" name="rag_soc" class="campo"> 
+                
+                <label for="ind" class="campo">Indirizzo: </label>
+                <input type="text" id="ind" name="indirizzo" class="campo">
+                
+                <label for="cit" class="campo">Città: </label>
+                <input type="text" id="cit" name="città" class="campo">
+                
+                <input type="submit" value="CERCA" class="avvio-ricerca">
 
             </form>
 
             <div class="risultati">
-                <p>Qui verranno mostrati i risultati</p>
+                <!-- <php if risultati>0 ?>
+                <p>Ecco i risultati della tua ricerca</p>
+                <php else if risultati==0 ?>
+                <p>La tua ricerca non ha prodotto risultati</p>
+                <php else ?> -->
+                <p>Nessuna ricerca effettuata</p>
             </div>
 
         </div>
