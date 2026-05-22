d9ca4ea8b5c20d6eb33bf2a46cbe1f97386695bd
jacopomarinelli <j.marinelli1@studenti.unibg.it>
2026-05-22 15:49:47 +0200

    Aggiornamento pagina clienti
    - modiificata zona di ricerca
    - aggiunto bottone da sistemare in risultati
    - aggiornamento file css

--- clienti.php
+++ clienti.php
@@ -8,32 +8,42 @@ include 'header.php';
             <form action="" method="GET" class="ricerca">
                 <p class="campo">Inserisci i dati per la ricerca</p>
 
-                <label for="cod" class="campo">Codice: </label>
-                <input type="text" id="cod" name="codice" class="campo">
+                <label for="cod" class="campo">Codice del cliente: </label>
+                <input type="text" id="cod" name="codice" class="text-area">
                 
-                <label for="cod_fis" class="campo">Codice fiscale: </label>
-                <input type="text" id="cod_fis" name="cod_fis" class="campo">
+                <label for="cod_fis" class="campo">Codice fiscale del cliente: </label>
+                <input type="text" id="cod_fis" name="cod_fis" class="text-area">
                 
-                <label for="rag_soc" class="campo">Ragione sociale: </label>
-                <input type="text" id="rag_soc" name="rag_soc" class="campo"> 
+                <label for="rag_soc" class="campo">Nome dell'azienda: </label>
+                <input type="text" id="rag_soc" name="rag_soc" class="text-area"> 
                 
                 <label for="ind" class="campo">Indirizzo: </label>
-                <input type="text" id="ind" name="indirizzo" class="campo">
+                <input type="text" id="ind" name="indirizzo" class="text-area">
                 
                 <label for="cit" class="campo">Città: </label>
-                <input type="text" id="cit" name="città" class="campo">
+                <input type="text" id="cit" name="città" class="text-area">
                 
-                <input type="submit" value="CERCA" class="avvio-ricerca">
+                <input type="submit" id="avvio" value="CERCA" class="avvio-ricerca">
 
             </form>
 
-            <div class="risultati">
-                <!-- <php if risultati>0 ?>
-                <p>Ecco i risultati della tua ricerca</p>
-                <php else if risultati==0 ?>
-                <p>La tua ricerca non ha prodotto risultati</p>
-                <php else ?> -->
-                <p>Nessuna ricerca effettuata</p>
+            <div class="mostra-risultati">
+                
+                <div class="campo-ricerca">
+                    <h3>CLIENTI</h3>
+                </div>
+
+                <div class="risultati">
+                    <!-- <php if risultati>0 ?>
+                    <p>Ecco i risultati della tua ricerca</p>
+                    <php else if risultati==0 ?>
+                    <p>La tua ricerca non ha prodotto risultati</p>
+                    <php else ?> -->
+                    <p>Nessuna ricerca effettuata</p>
+                </div>
+
+                <button type="button" class="nuova-ricerca">NUOVA RICERCA</button>
+
             </div>
 
         </div>
