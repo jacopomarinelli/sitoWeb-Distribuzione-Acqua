e6a365f7ebc04ab0955d8cf339373e9e5f1989cd
jacopomarinelli <j.marinelli1@studenti.unibg.it>
2026-05-25 13:09:47 +0200

    Aggiornamento clienti
    - creata tabella per mostrare risultati query
    - tolti due pulsanti
    - aggiornato css

--- clienti.php
+++ clienti.php
@@ -23,10 +23,7 @@ include 'header.php';
                 <label for="cit" class="campo">Città: </label>
                 <input type="text" id="cit" name="città" class="text-area">
                 
-                <div class="pulsanti-ricerca">
-                    <input type="submit" id="svuota" value="SVUOTA CAMPI" class="svuota-ricerca">
-                    <input type="submit" id="avvio" value="CERCA" class="avvio-ricerca">
-                </div>
+                <input type="submit" id="avvio" value="CERCA" class="avvio-ricerca">
 
             </form>
 
@@ -41,11 +38,18 @@ include 'header.php';
                     <p>Ecco i risultati della tua ricerca</p>
                     <php else if risultati==0 ?>
                     <p>La tua ricerca non ha prodotto risultati</p>
-                    <php else ?> -->
-                    <p>Nessuna ricerca effettuata</p>
+                    <php else ?>
+                    <p>Nessuna ricerca effettuata</p> -->
+                    <table>
+                        <tr>
+                            <th>Codice cliente</th>
+                            <th>Codice fiscale</th>
+                            <th>Ragione sociale</th>
+                            <th>Indirizzo</th>
+                            <th>Città</th>
+                        </tr>
+                    </table>
                 </div>
-
-                <button type="button" class="nuova-ricerca">NUOVA RICERCA</button>
                 
             </div>
 
