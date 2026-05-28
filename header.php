48af41eecedded7dcf599b60001a0318454c4634
jacopomarinelli <j.marinelli1@studenti.unibg.it>
2026-05-28 18:14:11 +0200

    Creazione utenze e aggiornamento
    -creata pagina utenze
    -modificato header
    -creata cartella css per css
    -aggiornato css

--- header.php
+++ header.php
@@ -1,23 +1,23 @@
 <!-- questo file va incluso in tutte le pagine -->
 <html>
     <head>
-        <title>Nome sito</title>
-        <link rel="stylesheet" href="stile.css">
+        <title>Home</title>
+        <link rel="stylesheet" href="css/stile.css">
     </head>
     <body>
         
         <div class="header">
             <div class="banner">
-                <h1>Nome sito</h1>
+                <h1>Aquabear</h1>
             </div>
 
             <nav>
                 <ul>
                     <li><a href="home.php">Home</a></li>
                     <li><a href="clienti.php">Clienti</a></li>
-                    <li><a href="">Pagina2</a></li>
-                    <li><a href="">Pagina3</a></li>
-                    <li><a href="">Pagina4</a></li>
+                    <li><a href="utenze.php">Utenze</a></li>
+                    <li><a href="">Letture</a></li>
+                    <li><a href="">Fatture</a></li>
                 </ul>
             </nav>
         </div>
---
+++ utenze.php
@@ -0,0 +1,71 @@
+<?php
+include 'header.php';
+?>
+<!-- Sezione contenente il codice della pagina -->
+
+        <div class="utenze">
+
+            <form action="" method="GET" class="ricerca">
+                <label for="cod_ut" class="campo">Codice dell'utenza: </label>
+                <input type="text" id="cod_ut" name="codice" class="text-area">
+                
+                <label for="cod_cli" class="campo">Codice del cliente: </label>
+                <input type="text" id="cod_cli" name="cod_fis" class="text-area">
+                
+                <label for="ind" class="campo">Indirizzo: </label>
+                <input type="text" id="ind" name="indirizzo" class="text-area">
+                
+                <label class="campo">Città: </label>
+                <input type="text" id="cit" name="città" class="text-area">
+                
+                <div class="attività">
+                    <label class="campo">Stato: </label>
+                    <input type="radio" id="attiva" name="stato" value="attiva">
+                    <label for="attiva">Attiva</label>
+                    <input type="radio" id="disattiva" name="stato" value="disattiva">
+                    <label for="disattiva">Disattiva</label>
+                </div>
+
+                <div class="date-area">
+                    <div class="campo-data">
+                        <label for="data_ap" class="campo">Data apertura: </label>
+                        <input type="date" id="data_ap" name="data_ap" class="data-area">
+                    </div>
+                    <div class="campo-data">
+                        <label for="data_ch" class="campo">Data chiusura: </label>
+                        <input type="date" id="data_ch" name="data_ch" class="date-area">
+                    </div>
+                </div>
+                
+                <input type="submit" id="avvio" value="CERCA" class="avvio-ricerca">
+
+            </form>
+
+            <div class="mostra-risultati">
+                
+                <div class="campo-ricerca">
+                    <h3>UTENZE</h3>
+                </div>
+
+                <div class="risultati">
+                    <table>
+                        <tr>
+                            <th id="col_cod_utenza">Codice utenza</th>
+                            <th id="col_cod_cliente">Codice cliente</th>
+                            <th id="col_data_apertura">Data apertura</th>
+                            <th id="col_indirizzo">Indirizzo</th>
+                            <th id="col_città">Città</th>
+                            <th id="col_stato">Stato</th>
+                            <th id="col_data_chiusura">Data chiusura</th>
+                        </tr>
+                    </table>
+                </div>
+                
+            </div>
+
+        </div>
+
+<!--  -->
+<?php
+include 'footer.php';
+?>
