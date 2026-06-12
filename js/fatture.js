bec4c3742fb111f003b3af485a7fafa07715e0b0
jacopomarinelli <j.marinelli1@studenti.unibg.it>
2026-06-13 00:32:56 +0200

    Prima versione popup aggiunta fattura
    - prima versione da sistemare

--- js/fatture.js
+++ js/fatture.js
@@ -20,3 +20,44 @@ function verificaPrezzoTotale(event) {
         apriPopup();
     }
 }
+
+/* FUNZIONI CREAZIONE FATTURA */
+function inserisciFormNelPopup() {
+    document.getElementById("messaggio_popup").innerHTML = `
+        <form action="" method="GET" class="ricerca-popup" onsubmit="verificaPrezzoTotale(event)">
+            
+            <label for="num_fat" class="campo">Numero della fattura: </label>
+            <input type="text" id="num_fat" name="numero_fattura" class="text-area">
+
+            <label for="data_fat" class="campo">Data: </label>
+            <input type="date" id="data_fat" name="data_fattura" class="text-area">
+
+            <div class="prezzo-fattura">
+
+                <div class="campo-prezzo">
+                    <label for="imp" class="campo">Imponibile: </label>
+                    <input type="text" id="imp" name="imponibile" class="text-area">
+                </div>
+
+                <div class="campo-prezzo">
+                    <label for="iva" class="campo">Iva: </label>
+                    <input type="text" id="iva" name="iva" class="text-area">
+                </div>
+
+                <div class="campo-prezzo">
+                    <label for="cos_tot" class="campo">Totale: </label>
+                    <input type="text" id="cos_tot" name="totale" class="text-area">
+                </div>
+
+            </div>
+
+            <input type="submit" value="AGGIUNGI" class="avvio-ricerca-popup">
+
+        </form>
+    `;
+}
+
+function creaFattura() {
+    inserisciFormNelPopup();
+    apriPopup();
+}
