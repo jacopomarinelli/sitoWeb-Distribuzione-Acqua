87f7d0d8fdf0cdc3286dd9c1e8f7bd2b60b116b4
jacopomarinelli <j.marinelli1@studenti.unibg.it>
2026-06-13 00:06:41 +0200

    Aggiunta popup

--- footer.php
+++ footer.php
@@ -4,5 +4,13 @@
                 Questo sito è stato creato per un progetto universitario, i dati presenti non sono reali e non rappresentano la realtà.
             </p>
         </footer>
+
+        <!-- popup presente in tutte le pagine -->
+        <div id="popup" class="finestra-popup" onclick="chiudiSeFuori(event)">
+            <div class="contenuto-popup">
+                <p id="messaggio_popup"></p>
+            </div>
+        </div>
+
     </body>
 </html>
