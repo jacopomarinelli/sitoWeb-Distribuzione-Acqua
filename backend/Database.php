ba660ce0bbe4a2fd8a06474ed2ed9a3f59f9ce4f
DanieleSalogni <d.salogni@studenti.unibg.it>
2026-06-18 18:13:26 +0200

    Update Database.php

--- backend/Database.php
+++ backend/Database.php
@@ -1,17 +1,32 @@
 <?php
 
 class Database {
+
     private static ?PDO $connection = null;
+
+    // CAMBIA QUI: "locale" oppure "altervista"
+    private static string $env = "locale";
 
     public static function getConnection(): PDO {
 
         if (self::$connection === null) {
 
-            self::$connection = new PDO(
-                "mysql:host=localhost;dbname=my_aquabear;charset=utf8",
-                "aquabear",
-                ""
-            );
+            if (self::$env === "altervista") {
+
+                self::$connection = new PDO(
+                    "mysql:host=localhost;dbname=my_aquabear;charset=utf8",
+                    "aquabear",
+                    ""
+                );
+
+            } else { // locale (XAMPP)
+
+                self::$connection = new PDO(
+                    "mysql:host=localhost;dbname=my_aquabear;charset=utf8",
+                    "root",
+                    ""
+                );
+            }
 
             self::$connection->setAttribute(
                 PDO::ATTR_ERRMODE,
